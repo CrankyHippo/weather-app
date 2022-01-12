3ea7b067a5f710aa77b8d44dc6e76cfc383841cc
Bryan Burnett <crankyhippo@gmail.com>
2022-01-11 19:50:28 -0800

    Minor display changes

--- src/components/CitySelector.js
+++ src/components/CitySelector.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {Row, Col, FormControl, Button} from 'react-bootstrap';
 
-
 const CitySelector = ({onSearch}) => {
     const [city, setCity] = useState('');
 
@@ -9,14 +8,14 @@ const CitySelector = ({onSearch}) => {
       <>
         <Row>
           <Col>
-            <h1>Search Your City</h1>
+            <h1>Search Weather</h1>
           </Col>
         </Row>
 
         <Row>
           <Col xl={5}>
             <FormControl
-              placeholder="Enter city"
+              placeholder="Enter city name... For example &quot;San Francisco&quot;"
               onChange={(event) => setCity(event.target.value)}
               value={city}
             />
@@ -25,7 +24,7 @@ const CitySelector = ({onSearch}) => {
 
         <Row>
           <Col>
-            <Button className="button" onClick={() => onSearch(city)}>Check Weather</Button>
+            <Button className="btn btn-dark" onClick={() => onSearch(city)}>Check Weather</Button>
           </Col>
         </Row>
       </>
