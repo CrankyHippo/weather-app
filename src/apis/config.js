49e68a712e2eddd8880f59e5d211af2b706fe647
Bryan Burnett <crankyhippo@gmail.com>
2022-01-14 18:30:13 -0800

    https update

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,2 +1,2 @@
 export const API_KEY = '21b326beff5477743a5f21eb88793ada';
-export const API_BASE_URL = 'http://api.openweathermap.org/';
+export const API_BASE_URL = 'https://api.openweathermap.org/';
